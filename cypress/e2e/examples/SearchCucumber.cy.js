1d00039d2a64574cde3a06fc90257e1618a5cc5a
ismailozkanlaw <ismailozkanlaw@gmail.com>
2023-07-07 08:16:17 +0300

    23. Understanding the difference between Jquery methods and cypress commands

--- cypress/e2e/examples/SearchCucumber.cy.js
+++ cypress/e2e/examples/SearchCucumber.cy.js
@@ -18,7 +18,19 @@ describe('Search Features Tests', () => {
         cy.get(':nth-child(3) > .product-action > button').click();
         cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click();
 
+        // find Cashews and click add to cart button under it
+        cy.get('.products').find('.product').each(($el, index,list) => {
+            const textOfVeg = $el.find('h4.product-name').text();
+            if (textOfVeg.includes('Cashews')) {
+                cy.wrap($el).find('button').click();
+            }
+        })
 
-
+        cy.get('.brand').then((logoelement) => {
+            cy.log(logoelement.text());
+        })
+        //const logoText = cy.get('.brand').then(() => {
+        //    cy.log(logoText.text());
+        //});
     })
 })
