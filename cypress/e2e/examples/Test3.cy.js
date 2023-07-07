b36d5f50039b5b1688cbdf1e82d4612633da49fd
ismailozkanlaw <ismailozkanlaw@gmail.com>
2023-07-07 13:44:11 +0300

    29. static dropdowns

--- cypress/e2e/examples/Test3.cy.js
+++ cypress/e2e/examples/Test3.cy.js
@@ -16,6 +16,17 @@ describe('Third Test Suite', () => {
             cy.wrap(option).check();
         })
 
+        //29. Handling static dropdowns using select command with Cypress
+        // There are two types dropdowns which have different features
+        // Dynamic dropdowns, Static dropdowns(options is displayed when click it)
+        //Static select dropdown  --> value or text or index(which starts from 1)
+        cy.get('#dropdown-class-example').select('Option1');
+        cy.wait(1000);
+        cy.get('#dropdown-class-example').select('option2');
+        cy.wait(1000);
+        cy.get('select').select(3).should('have.value', 'option3');
+
+
 
 
 
