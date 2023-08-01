ee1d0b61e9b0dffcdeec922e22f4b29bf1a4b49b
ismailozkanlaw <ismailozkanlaw@gmail.com>
2023-08-01 19:17:57 +0300

    cucumber bdd configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('cypress');
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", browserify.default(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 
 module.exports = defineConfig({
   projectId: "xuha8i",
@@ -12,10 +24,8 @@ module.exports = defineConfig({
     openMode: 0,
   },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    specPattern: 'cypress/e2e/examples/*.js',
+    setupNodeEvents,
+    specPattern: 'cypress/e2e/examples/BDD/*.feature',
     screenshotOnRunFailure: true,
     screenshotsFolder: 'cypress/screenshotsForFailures',
     video: false,
--- cypress/e2e/examples/TestInExamples1.cy.js
+++
@@ -1,7 +0,0 @@
-describe('My First Test Suite in examples folder', () => {
-    it('My First Test Case', () => {
-        // test steps
-        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
-    })
-
-})
